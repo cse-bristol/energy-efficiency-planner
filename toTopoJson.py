86189a92a055fe8e4aa27c802f349d26b45edf53
Glenn Searby <glenn.searby@gmail.com>
2014-02-14 11:48:20 +0000

    Improved the topojson conversion script: better paths, directory cleanup etc.

--- toTopoJson.py
+++ toTopoJson.py
@@ -1,14 +1,16 @@
-from os import listdir, path, makedirs
+from os import listdir, path, makedirs, getenv
 from zipfile import ZipFile
 from subprocess import call
 from shutil import rmtree
 
 # Depends on topoJSON being installed, see: https://github.com/mbostock/topojson/wiki/Installation
+# This in turn depends on Python 2.7 being installed and on the path. Bleh.
 
-inDir = "~/Dropbox/CSE/Data"
+inDir = getenv('HOME') + "/Dropbox/CSE/Data"
 tempDir = "./temp"
 outDir = "./data"
 topoJson = "C:/node_modules/.bin/topojson.cmd"
+simplify = 0.2;
 
 if not path.exists(tempDir):
     makedirs(tempDir)
@@ -27,7 +29,7 @@ for file in listdir(inDir):
 
             shpFilePath = tempDir + "/" + shpFile
             outFilePath = outDir + "/" + name + ".json"
-            call([topoJson, shpFilePath, '--out', outFilePath])
+            call([topoJson, shpFilePath, '--simplify-proportion', str(simplify), '--out', outFilePath])
         except KeyError as e:
             print("Failed to convert to topojson: " + file + " error: " + e.message)
 
