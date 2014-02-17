28a322c95d99575a52078a81fc62dab7ae13743e
glenns <Glenn.Searby@cse.org.uk>
2014-02-17 16:56:55 +0000

    Performance improvement: data is no longer reloaded each time you pan or zoom. Also, added some colouring to the different regions.

--- map.js
+++ map.js
@@ -5,21 +5,21 @@
 var tileScale = 256;
 var width = Math.max(960, window.innerWidth) - 10;
 var height = Math.max(500, window.innerHeight) - 10;
-var startCoordinates = [-2.5833, 51.4500];
+var startCoordinates = [-3.0, 54.0];
 var zoom = 15;
 
 var tiler = d3.geo.tile()
-    .size([width, height]);
+	.size([width, height]);
 
 var projection = d3.geo.mercator()
-    .center(startCoordinates);
+	.center(startCoordinates);
 
 var zoomer = d3.behavior.zoom()
-    .scale(1 << zoom)
-    .translate([width / 2, height / 2]);
+	.scale(1 << zoom)
+	.translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
-    .projection(projection);
+	.projection(projection);
 
 d3.select("body")
     .append("svg")
@@ -30,9 +30,9 @@ d3.select("body")
     .call(zoomer);
 
 var roads = d3.select("svg").append("g")
-    .attr("id", "roads")
-    .attr("width", width)
-    .attr("height", height);
+	.attr("id", "roads")
+	.attr("width", width)
+	.attr("height", height);
 
 var tileURL = function(d) {
     var x = d[0];
@@ -43,80 +43,69 @@ var tileURL = function(d) {
 
 var layers = d3.map({
     //"data/example.json": "example",
-    "data/uk.json" : "subunits",
+    "data/uk.json" : "subunits", // TODO remove thius and put in national level boundaries
     //"data/Gov_Office_Region_DEC_2010_EN_Gen_Clip.json" : "Gov_Office_Region_DEC_2010_EN_Gen_Clip",
     //"data/County_Unitary_Auth_DEC_2012_EW_Gen_Clip.json" : "County_Unitary_Auth_DEC_2012_EW_Gen_Clip",
-    //"data/Middle_Layer_SOA_2011_EW_Gen_Clip.json" : "Middle_Layer_SOA_2011_EW_Gen_Clip",
-    //"data/Lower_Layer_SOA_2011_EW_Gen_Clip.json" : "Lower_Layer_SOA_2011_EW_Gen_Clip",
-    "data/Wards_DEC_2012_GB_Gen_Clip.json" : "Wards_DEC_2012_GB_Gen_Clip",
+    "data/Middle_Layer_SOA_2011_EW_Gen_Clip.json" : "Middle_Layer_SOA_2011_EW_Gen_Clip"
+    //"data/Lower_Layer_SOA_2011_EW_Gen_Clip.json" : "Lower_Layer_SOA_2011_EW_Gen_Clip"
+    //"data/Wards_DEC_2012_GB_Gen_Clip.json" : "Wards_DEC_2012_GB_Gen_Clip"
 });
 
-layers.forEach(function(k, v){
+var loaded = d3.map();
+
+layers.forEach(function(filePath, layerID){
+    d3.json(filePath, function(error, data) {
+	if (error) {
+	    console.log("Couldn't load " + error);
+	} else {
+	    var shapes = data.objects[layerID];
+	    var topojsonShapes = topojson.feature(data, shapes);
+	    if (topojsonShapes.features) {
+		topojsonShapes = topojsonShapes.features;
+	    } else {
+		topojsonShapes = [topojsonShapes];
+	    }
+
+	    loaded.set(layerID, topojsonShapes);
+	    redraw();
+	}
+    });
+});
+
+layers.forEach(function(filePath, layerID){
     d3.select("svg").append("g")
-	.attr("id", v)
+	.attr("id", layerID)
 	.attr("width", width)
 	.attr("height", height);
 });
+
+var colour = function(){
+    var colours = d3.scale.category20();
+
+    return {
+	/* Items next to each other in a list will get different colours. */
+	byIndex : function(data, index) {
+	    return colours(index);
+	}
+    };
+}();
 
 var redraw = function() {
 
     projection.scale(zoomer.scale() / 2 / Math.PI)
 	.translate(zoomer.translate());
-
-    // var drawTile = function(tileData) {
-    // 	var onTileLoad = function(error, json) {
-    // 	    var pieces = g.selectAll("path")
-    // 		    .data(json.features.sort(function(a, b){
-    // 			return a.properties.sort_key - b.properties.sort_key;
-    // 		    }));
-
-    // 	    pieces.exit().remove();
-
-    // 	    pieces.enter()
-    // 		.append("path")
-    // 		.attr("class", function(d){return d.properties.kind;})
-    // 		.attr("d", path);
-    // 	};
-
-    // 	var g = d3.select(this);
-    // 	d3.json(tileURL(tileData), onTileLoad);
-    // };
-
-    // var tiles = roads.selectAll("g")
-    // 	.data(tiler
-    // 	      .scale(projection.scale() * 2 * Math.PI)
-    // 	      .translate(projection([0, 0])));
-    // tiles.enter()
-    // 	.append("g")
-    // 	.attr("class", "tile")
-    // 	.each(drawTile);
     
-    // tiles.exit().remove();
-    
-    layers.forEach(function(k, v){
-	d3.json(k, function(error, data) {
-	    if (error) {
-		console.log("Couldn't load " + error);
-	    } else {
-		var shapes = data.objects[v];
-		var topojsonShapes = topojson.feature(data, shapes);
-		if (topojsonShapes.features) {
-		    topojsonShapes = topojsonShapes.features;
-		} else {
-		    topojsonShapes = [topojsonShapes];
-		}
+    loaded.forEach(function(layerID, topojsonShapes){
+	var paths = d3.select("g#" + layerID)
+		.selectAll("path")
+		.data(topojsonShapes);
+	paths.enter()
+	    .append("path")
+	    .attr("d", path)
+	    .attr("fill", colour.byIndex);
 
-		var paths = d3.select("g#" + v)
-		    .selectAll("path")
-		    .data(topojsonShapes);
-		paths.enter()
-		    .append("path")
-		    .attr("d", path);
+	paths.attr("d", path);
 
-		paths.attr("d", path);
-
-	    }
-	});
     });
 };
 
