5ab98b01f117ac298f83ff459c0923b40a7133e5
Glenn Searby <glenn.searby@gmail.com>
2014-03-06 11:45:36 +0000

    Rewrote toTopoJson.py. Now uses a different folder structure. Handles layers in shapefiles. Removed all the special postcode handling code for now.

--- toTopoJson.py
+++ toTopoJson.py
@@ -1,98 +1,110 @@
 from os import listdir, path, makedirs, getenv, remove
 from zipfile import ZipFile
-from subprocess import call
+from subprocess import call, check_output
 from shutil import rmtree
-from json import load
+from itertools import chain
+from re import compile
 
 import postcodes
 
+# Run this script with Python 3.
+
 # Depends on topoJSON being installed, see: https://github.com/mbostock/topojson/wiki/Installation
-# This in turn depends on Python 2.7 being installed and on the path. Bleh.
+# This in turn depends on Python 2.7 being installed and on the path.
 
 # Depends on ogr2ogr being installed and the GDAL_DATA environment variable being set.
 
-inDir = "./data/raw"
+inDir = "./data/geometry"
 tempDir = "./temp"
 outDir = "./data/processed"
 outFile = outDir + "/merged.json"
 topoJson = "topojson"
 ogr2ogr = "ogr2ogr"
+ogrinfo = "ogrinfo"
 simplify = 0.5;
 
-def sphericalGeoJSON(inputPath, geoJSONPath):
-    geoJSON(inputPath, geoJSONPath, "EPSG:4326")
+def sphericalGeoJSON(inputPath, outPath, layer=None):
+    geoJSON(inputPath, outPath, layer, "EPSG:4326") # Lat and Long
 
-def flatGeoJSON(inputPath, geoJSONPath):
-    geoJSON(inputPath, geoJSONPath, "EPSG:27700") # OS Map
+def flatGeoJSON(inputPath, outPath, layer=None):
+    geoJSON(inputPath, outPath, layer, "EPSG:27700") # OS Map
 
-def geoJSON(inputPath, geoJSONPath, targetSpatialReferenceSystem):
-    call([ogr2ogr, "-f", "GeoJSON", geoJSONPath, inputPath, "-t_srs", targetSpatialReferenceSystem])
+def geoJSON(inputPath, outPath, layer, targetSpatialReferenceSystem):
+    args = [ogr2ogr, "-t_srs", targetSpatialReferenceSystem, "-f", "GeoJSON", outPath, inputPath]
 
-if not path.exists(tempDir):
-    makedirs(tempDir)
+    if layer:
+        args.append(layer)
+
+    call(args)
+
+def handleZip(name, inFile):
+    z = ZipFile(inFile, 'r')
+
+    extracted = tempdir + "/" + name
+    makedirs(extracted)
+    z.extractall(extracted)
+    return loadgeometry(name, extracted)
+    
+
+ogrinfoPattern = compile(r"\d+: (.*) \(\w+\)")
+def handleShp(name, inFile):
+    def getLayers():
+        def parseLine(l):
+           m = ogrinfoPattern.match(l)
+           if m:
+               return m.group(1)
+
+        layers = check_output([ogrinfo, inFile]).decode().split('\n')
+        for l in layers:
+            parsed = parseLine(l)
+            if parsed:
+                yield parsed
+
+
+    def process(l):
+        outFile = tempDir + "/" + name + "_" + l + ".json"
+        sphericalGeoJSON(inFile, outFile, l)
+        return outFile
+        
+    return [process(l) for l in getLayers()]
+
+
+def handleGeoJSON(geometryName, inFile):
+    outFile = tempDir + "/" + geometryName + ".json"
+    sphericalGeoJSON(inFile, outFile)
+    return [outFile]
+
+handlers = {
+    ".zip": handleZip,
+    ".shp": handleShp,
+    ".json": handleGeoJSON
+}
+
+def loadgeometry(geometry, d):
+    for f in listdir(d):
+        _, ext = path.splitext(f)
+        if ext in handlers:
+            print("Handling geometry " + geometry + " as " + ext)
+            return handlers[ext](geometry, d + "/" + f)
+
+
+
+if path.exists(tempDir):
+    rmtree(tempDir)
+    
+makedirs(tempDir)
 
 if not path.exists('./data'):
     makedirs('./data')
 
-geoJSONFiles = []
+processed = { g : loadgeometry(g, inDir + "/" + g) for g in listdir(inDir) }
+geoJSONFiles = list(chain(*processed.values()))
 
-for file in listdir(inDir):
-    if file.endswith(".zip"):
-        zip = ZipFile(inDir + "/" + file, 'r')
-        name, ext = path.splitext(file)
-
-        try:
-            shpFile = "zipfolder/" + name + ".shp"
-            zip.getinfo(shpFile)
-            zip.extractall(tempDir)
-            
-            shpPath = tempDir + "/" + shpFile
-            geoJSONPath = tempDir + "/" + name + ".geojson"
-            
-            if path.exists(geoJSONPath):
-                remove(geoJSONPath)
-
-            if name == "Lower_Layer_SOA_2011_EW_Gen_Clip":
-                print("Loading LSOA boundaries as OSMap format.")
-                flatGeoJSON(shpPath, geoJSONPath)
-
-                with open(geoJSONPath) as f:
-                    lsoas = load(f)
-
-                postcodesByLSOA = postcodes.extract(temp = tempDir)
-
-                # TODO match LSOAs to polygons
-
-                print("Creating tesselated postcode regions within LSOA boundaries.")
-                postcodeJSON = tempDir + "/postcodes.geojson"
-                with open(postcodeJSON, 'w') as f:
-                    i = 0
+if geoJSONFiles:
+    print("Combining geoJSON files and converting to topoJSON.")
+    call([topoJson, '--simplify-proportion', str(simplify), '--properties', '--out', outFile] + geoJSONFiles)
+else:
+    print("Failed to import any geometries.")
     
-                    postcodes.jsonHead(f)
-                    for lsoa, pcdList in postcodesByLSOA.items():
-                        if pcdList:
-                            postcodes.json(postcodes.voronoi(pcdList, lsoa), f, first=(i==0))
-                            i += 1
-                        else:
-                            print("Skipping empty LSOA " + lsoa)
-                    postcodes.jsonFoot(f)
-
-                # Currently not working: runs out of memory.
-                # geoJSONFiles.append(postcodeJSON)
-
-                # TODO modify the LSOAs and reproject them as spherical coordinates
-
-
-            else:
-                print("Converting shapefile to spherical coordinates " + shpPath)
-                sphericalGeoJSON(shpPath, geoJSONPath)
-                geoJSONFiles.append(geoJSONPath)
-
-        except KeyError as e:
-            print("Failed to convert to geojson: " + file + " error: " + str(e))
-
-print("Combining geoJSON files and converting to topoJSON.")
-call([topoJson, '--simplify-proportion', str(simplify), '--properties', '--out', outFile] + geoJSONFiles)
-
 print("Cleaning up temp directory.")
 rmtree(tempDir)
