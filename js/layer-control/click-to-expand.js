1f04f450a8aff15fa734a0443a119c9426462f8a
Glenn Searby <glenn.searby@gmail.com>
2015-07-13 17:08:27 +0100

    Fixed click-to-expand.

--- js/layer-control/click-to-expand.js
+++ js/layer-control/click-to-expand.js
@@ -11,23 +11,21 @@ var d3 = require("d3"),
  */
 module.exports = function(selection) {
     selection
+	.classed("can-expand", true)
 	.style("overflow", "hidden")
-	.style("height", "1em")
 	.on("click", function(d, i) {
-	    if (expander.text() === expand) {
-		expander.text(contract);
-		d3.select(this)
-		    .style("height", null);
-		
-	    } else {
-		expander.text(expand);
-		d3.select(this)
-		    .style("height", "1em");
-	    }
+	    var current = d3.select(this),
+		expanded = current.classed("expanded"),
+		shouldExpand = !expanded;
+
+	    current
+		.classed("expanded", shouldExpand)
+		.select(".expander")
+		.text(shouldExpand ? contract : expand);
 	});
 
-    var expander = selection
-	    .append("span")
-	    .classed("expander", true)
-	    .text(expand);
+    selection
+	.append("span")
+	.classed("expander", true)
+	.text(expand);
 };
